f51d295fb92086a0293b3078e9d4d078481dc3af
Sahil al farib <117147361+sahilaf@users.noreply.github.com>
2025-09-27 04:34:26 +0600

    Updated version for openface
    Updated installation script for OpenFace to be compatible with Colab and Ubuntu 22.04. Removed outdated dependencies and streamlined installation process.

--- install.sh
+++ install.sh
@@ -1,101 +1,47 @@
 #!/bin/bash
 #==============================================================================
-# Title: install.sh
-# Description: Install everything necessary for OpenFace to compile. 
-# Will install all required dependencies, only use if you do not have the dependencies
-# already installed or if you don't mind specific versions of gcc,g++,cmake,opencv etc. installed
-# Author: Daniyal Shahrokhian <daniyal@kth.se>, Tadas Baltrusaitis <tadyla@gmail.com>
-# Date: 20190630
-# Version : 1.03
-# Usage: bash install.sh
+# Title: install.sh (Colab/Ubuntu 22.04 compatible)
+# Description: Install dependencies and build OpenFace
+# Author: Modified for 2025 by ChatGPT
 #==============================================================================
 
-# Exit script if any command fails
-set -e 
+set -e
 set -o pipefail
 
-if [ $# -ne 0 ]
-  then
-    echo "Usage: install.sh"
-    exit 1
-fi
-
-# Essential Dependencies
-echo "Installing Essential dependencies..."
-
-# If we're not on 18.04
+echo "Updating system..."
 sudo apt-get -y update
+sudo apt-get -y upgrade
 
-if [[ `lsb_release -rs` != "18.04" ]]
-  then   
-    echo "Adding ppa:ubuntu-toolchain-r/test apt-repository "
-    sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-    sudo apt-get -y update
-fi
+echo "Installing essential dependencies..."
+sudo apt-get -y install build-essential cmake pkg-config \
+    libopenblas-dev liblapack-dev \
+    libgtk-3-dev libavcodec-dev libavformat-dev libswscale-dev \
+    libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-dev \
+    git unzip wget
 
-sudo apt-get -y install build-essential
-sudo apt-get -y install gcc-8 g++-8
+echo "Installing OpenCV (system package)..."
+sudo apt-get -y install libopencv-dev python3-opencv
 
-# Ubuntu 16.04 does not have newest CMake so need to build it manually
-if [[ `lsb_release -rs` < "18.04" ]]; then   
-  mkdir -p cmake_tmp
-  cd cmake_tmp
-  wget https://cmake.org/files/v3.10/cmake-3.10.1.tar.gz
-  tar -xzvf cmake-3.10.1.tar.gz
-  cd cmake-3.10.1/
-  ./bootstrap
-  make -j4
-  sudo make install
-  cd ../..
-  sudo rm -r cmake_tmp
-  export PATH=/usr/local/bin:$PATH
-else
-  sudo apt-get -y install cmake
-fi
-
-sudo apt-get -y install zip
-sudo apt-get -y install libopenblas-dev liblapack-dev
-sudo apt-get -y install libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev
-sudo apt-get -y install libtbb2 libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev
-echo "Essential dependencies installed."
-
-# OpenCV Dependency
-echo "Downloading OpenCV..."
-wget https://github.com/opencv/opencv/archive/4.1.0.zip
-unzip 4.1.0.zip
-cd opencv-4.1.0
-mkdir -p build
-cd build
-echo "Installing OpenCV..."
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D WITH_CUDA=OFF -D BUILD_SHARED_LIBS=OFF ..
-make -j4
+echo "Installing dlib (modern build)..."
+# Remove old dlib if exists
+pip uninstall -y dlib || true
+# Build from source (Colab needs this for OpenFace compatibility)
+wget http://dlib.net/files/dlib-19.24.tar.bz2
+tar xf dlib-19.24.tar.bz2
+cd dlib-19.24
+mkdir build && cd build
+cmake .. -DDLIB_USE_CUDA=0 -DUSE_AVX_INSTRUCTIONS=1
+cmake --build . --config Release -j4
 sudo make install
+sudo ldconfig
 cd ../..
-rm 4.1.0.zip
-sudo rm -r opencv-4.1.0
-echo "OpenCV installed."
+rm -rf dlib-19.24 dlib-19.24.tar.bz2
 
-# dlib dependecy
-echo "Downloading dlib"
-wget http://dlib.net/files/dlib-19.13.tar.bz2;
-tar xf dlib-19.13.tar.bz2;
-cd dlib-19.13;
-mkdir -p build;
-cd build;
-echo "Installing dlib"
-cmake ..;
-cmake --build . --config Release;
-sudo make install;
-sudo ldconfig;
-cd ../..;    
-rm -r dlib-19.13.tar.bz2
-echo "dlib installed"
-
-# OpenFace installation
-echo "Installing OpenFace..."
+echo "Building OpenFace..."
 mkdir -p build
 cd build
-cmake -D CMAKE_CXX_COMPILER=g++-8 -D CMAKE_C_COMPILER=gcc-8 -D CMAKE_BUILD_TYPE=RELEASE ..
-make
+cmake -D CMAKE_BUILD_TYPE=Release ..
+make -j4
 cd ..
-echo "OpenFace successfully installed."
+
+echo "✅ OpenFace installation complete!"
